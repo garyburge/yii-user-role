57b477f3f325289856d9c0d7b0788c34428a877d
Gary Burge <garyburge@garyburge.com>
2013-06-21 05:13:10 -0400

    Changed AdminController to allow non-selection of user roles on edit user form

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -3,227 +3,227 @@
 class AdminController extends Controller
 {
 
-  public $defaultAction = 'admin';
-  private $_model;
+    public $defaultAction = 'admin';
+    private $_model;
 
-  /**
-   * @return array action filters
-   */
-  public function filters()
-  {
-    return CMap::mergeArray(parent::filters(), array(
-        'accessControl', // perform access control for CRUD operations
-    ));
-  }
-
-  /**
-   * Specifies the access control rules.
-   * This method is used by the 'accessControl' filter.
-   * @return array access control rules
-   */
-  public function accessRules()
-  {
-    return array(
-      array('allow', // allow admin user to perform 'admin' and 'delete' actions
-        'actions'=>array('admin', 'delete', 'create', 'update', 'view'),
-        //'users'=>UserModule::getAdmins(),
-        'roles'=>array('admin'),
-      ),
-      array('deny', // deny all users
-        'users'=>array('*'),
-      ),
-    );
-  }
-
-  /**
-   * Manages all models.
-   */
-  public function actionAdmin()
-  {
-    // create model for searching
-    $model = new User('search');
-
-    // clear any default values
-    $model->unsetAttributes();
-
-    // if paging info available...
-    if (isset($_GET['User'])) {
-      $model->attributes = $_GET['User'];
+    /**
+     * @return array action filters
+     */
+    public function filters()
+    {
+        return CMap::mergeArray(parent::filters(), array(
+            'accessControl', // perform access control for CRUD operations
+        ));
     }
 
-    $this->render('index', array(
-      'model'=>$model,
-    ));
-  }
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions'=>array('admin', 'delete', 'create', 'update', 'view'),
+                //'roles'=>array('admin'),
+                'users'=>UserModule::getAdmins(),
+            ),
+            array('deny', // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
 
-  /**
-   * Displays a particular model.
-   */
-  public function actionView()
-  {
-    $model = $this->loadModel();
-    $this->render('view', array(
-      'model'=>$model,
-    ));
-  }
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin()
+    {
+        // create model for searching
+        $model = new User('search');
 
-  /**
-   * Creates a new model.
-   */
-  public function actionCreate()
-  {
-    // create an empty model
-    $model = new User;
-    $profile = new Profile;
-    $role = new RoleSelectForm();
+        // clear any default values
+        $model->unsetAttributes();
 
-    // perform ajax validation
-    $this->performAjaxValidation(array($model, $profile));
-
-    // if form data available
-    if (isset($_POST['User'])) {
-      // copy form data to model
-      $model->attributes = $_POST['User'];
-      // set active key
-      $model->active_key = Yii::app()->controller->module->encrypting(microtime() . $model->password);
-      // copy profile data
-      $profile->attributes = $_POST['Profile'];
-      // set user id to zero for new user
-      $profile->user_id = 0;
-      // validate separately
-      if ($model->validate() && $profile->validate()) {
-        // encrypt password
-        $model->password = Yii::app()->controller->module->encrypting($model->password);
-        // save user
-        if ($model->save(false)) {
-          // copy new user id
-          $profile->user_id = $model->id;
-          // save profile
-          $profile->save(false);
-          // copy selections
-          $role->attributes = $_POST['RoleSelectForm'];
-          // create auth assignment for this user and each selected role
-          $role->assignUser($model->id);
+        // if paging info available...
+        if (isset($_GET['User'])) {
+            $model->attributes = $_GET['User'];
         }
-        // redirect to admin grid
-        $this->redirect(array('admin'));
-      } else {
-        // don't understand this
-        $profile->validate();
-      }
+
+        $this->render('index', array(
+            'model'=>$model,
+        ));
     }
 
-    // render form
-    $this->render('create', array(
-      'model'=>$model,
-      'profile'=>$profile,
-      'role'=>$role,
-    ));
-  }
+    /**
+     * Displays a particular model.
+     */
+    public function actionView()
+    {
+        $model = $this->loadModel();
+        $this->render('view', array(
+            'model'=>$model,
+        ));
+    }
 
-  /**
-   * Updates a particular model.
-   */
-  public function actionUpdate()
-  {
-    // create model for this user
-    $model = $this->loadModel();
-    // create profile
-    $profile = $model->profile;
-    // create role
-    $role = new RoleSelectForm($model->id);
+    /**
+     * Creates a new model.
+     */
+    public function actionCreate()
+    {
+        // create an empty model
+        $model = new User;
+        $profile = new Profile;
+        $role = new RoleSelectForm();
 
-    // do ajax validation
-    $this->performAjaxValidation(array($model, $profile));
+        // perform ajax validation
+        $this->performAjaxValidation(array($model, $profile));
 
-    // if form data available
-    if (isset($_POST['User'])) {
-      // copy user data
-      $model->attributes = $_POST['User'];
-      // copy profile data
-      $profile->attributes = $_POST['Profile'];
-      // copy role selection
-      $role->attributes = $_POST['RoleSelectForm'];
-
-      // validate
-      if ($model->validate() && $profile->validate()) {
-        // did editor change password?
-        $old_password = User::model()->notsafe()->findByPk($model->id);
-        if ($old_password->password != $model->password) {
-          // encrypt new password and key
-          $model->password = Yii::app()->controller->module->encrypting($model->password);
-          $model->active_key = Yii::app()->controller->module->encrypting(microtime() . $model->password);
+        // if form data available
+        if (isset($_POST['User'])) {
+            // copy form data to model
+            $model->attributes = $_POST['User'];
+            // set active key
+            $model->active_key = Yii::app()->controller->module->encrypting(microtime() . $model->password);
+            // copy profile data
+            $profile->attributes = $_POST['Profile'];
+            // set user id to zero for new user
+            $profile->user_id = 0;
+            // validate separately
+            if ($model->validate() && $profile->validate()) {
+                // encrypt password
+                $model->password = Yii::app()->controller->module->encrypting($model->password);
+                // save user
+                if ($model->save(false)) {
+                    // copy new user id
+                    $profile->user_id = $model->id;
+                    // save profile
+                    $profile->save(false);
+                    // copy selections
+                    $role->attributes = $_POST['RoleSelectForm'];
+                    // create auth assignment for this user and each selected role
+                    $role->assignUser($model->id);
+                }
+                // redirect to admin grid
+                $this->redirect(array('admin'));
+            } else {
+                // don't understand this
+                $profile->validate();
+            }
         }
-        // save user data
-        $model->save();
-        // save profile data
-        $profile->save();
-        // create new assignments
-        $role->assignUser($model->id);
-        // return to admin grid
-        $this->redirect(array('admin'));
-      } else {
-        // don't understand this
-        $profile->validate();
-      }
+
+        // render form
+        $this->render('create', array(
+            'model'=>$model,
+            'profile'=>$profile,
+            'role'=>$role,
+        ));
     }
 
-    // get roles assigned to this user
-    $role->getAssigned($model->id);
+    /**
+     * Updates a particular model.
+     */
+    public function actionUpdate()
+    {
+        // create model for this user
+        $model = $this->loadModel();
+        // create profile
+        $profile = $model->profile;
+        // create role
+        $role = new RoleSelectForm($model->id);
 
-    $this->render('update', array(
-      'model'=>$model,
-      'profile'=>$profile,
-      'role'=>$role,
-    ));
-  }
+        // do ajax validation
+        $this->performAjaxValidation(array($model, $profile));
 
-  /**
-   * Deletes a particular model.
-   * If deletion is successful, the browser will be redirected to the 'index' page.
-   */
-  public function actionDelete()
-  {
-    if (Yii::app()->request->isPostRequest) {
-      // we only allow deletion via POST request
-      $model = $this->loadModel();
-      $profile = Profile::model()->findByPk($model->id);
-      $profile->delete();
-      // TODO Delete AuthAssign records
-      $model->delete();
-      // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-      if (!isset($_POST['ajax']))
-        $this->redirect(array('/user/admin'));
-    } else {
-      throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
+        // if form data available
+        if (isset($_POST['User'])) {
+            // copy user data
+            $model->attributes = $_POST['User'];
+            // copy profile data
+            $profile->attributes = $_POST['Profile'];
+            // copy role selection
+            $role->attributes = (isset($_POST['RoleSelectForm']) ? $_POST['RoleSelectForm'] : array());
+
+            // validate
+            if ($model->validate() && $profile->validate()) {
+                // did editor change password?
+                $old_password = User::model()->notsafe()->findByPk($model->id);
+                if ($old_password->password != $model->password) {
+                    // encrypt new password and key
+                    $model->password = Yii::app()->controller->module->encrypting($model->password);
+                    $model->active_key = Yii::app()->controller->module->encrypting(microtime() . $model->password);
+                }
+                // save user data
+                $model->save();
+                // save profile data
+                $profile->save();
+                // create new assignments
+                $role->assignUser($model->id);
+                // return to admin grid
+                $this->redirect(array('admin'));
+            } else {
+                // don't understand this
+                $profile->validate();
+            }
+        }
+
+        // get roles assigned to this user
+        $role->getAssigned($model->id);
+
+        $this->render('update', array(
+            'model'=>$model,
+            'profile'=>$profile,
+            'role'=>$role,
+        ));
     }
-  }
 
-  /**
-   * Performs the AJAX validation.
-   * @param CModel the model to be validated
-   */
-  protected function performAjaxValidation($validate)
-  {
-    if (isset($_POST['ajax']) && $_POST['ajax'] === 'user-form') {
-      echo CActiveForm::validate($validate);
-      Yii::app()->end();
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     */
+    public function actionDelete()
+    {
+        if (Yii::app()->request->isPostRequest) {
+            // we only allow deletion via POST request
+            $model = $this->loadModel();
+            $profile = Profile::model()->findByPk($model->id);
+            $profile->delete();
+            // TODO Delete AuthAssign records
+            $model->delete();
+            // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+            if (!isset($_POST['ajax']))
+                $this->redirect(array('/user/admin'));
+        } else {
+            throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
+        }
     }
-  }
 
-  /**
-   * Returns the data model based on the primary key given in the GET variable.
-   * If the data model is not found, an HTTP exception will be raised.
-   */
-  public function loadModel()
-  {
-    if ($this->_model === null) {
-      if (isset($_GET['id']))
-        $this->_model = User::model()->notsafe()->findbyPk($_GET['id']);
-      if ($this->_model === null)
-        throw new CHttpException(404, 'The requested page does not exist.');
+    /**
+     * Performs the AJAX validation.
+     * @param CModel the model to be validated
+     */
+    protected function performAjaxValidation($validate)
+    {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'user-form') {
+            echo CActiveForm::validate($validate);
+            Yii::app()->end();
+        }
     }
-    return $this->_model;
-  }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     */
+    public function loadModel()
+    {
+        if ($this->_model === null) {
+            if (isset($_GET['id']))
+                $this->_model = User::model()->notsafe()->findbyPk($_GET['id']);
+            if ($this->_model === null)
+                throw new CHttpException(404, 'The requested page does not exist.');
+        }
+        return $this->_model;
+    }
 
 }
--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
-  class UserController extends Controller {
+class UserController extends Controller
+{
 
     /**
      * @var CActiveRecord the currently loaded data model instance.
@@ -10,10 +11,11 @@
     /**
      * @return array action filters
      */
-    public function filters() {
-      return CMap::mergeArray(parent::filters(), array(
-          'accessControl', // perform access control for CRUD operations
-      ));
+    public function filters()
+    {
+        return CMap::mergeArray(parent::filters(), array(
+            'accessControl', // perform access control for CRUD operations
+        ));
     }
 
     /**
@@ -21,58 +23,62 @@
      * This method is used by the 'accessControl' filter.
      * @return array access control rules
      */
-    public function accessRules() {
-      return array(
+    public function accessRules()
+    {
+        return array(
 //        array('allow', // allow all users to perform 'index' and 'view' actions
 //          'actions' => array('index', 'view'),
 //          'users' => array('*'),
 //        ),
-        array('deny', // deny all users
-          'users' => array('*'),
-        ),
-      );
+            array('deny', // deny all users
+                'users'=>array('*'),
+            ),
+        );
     }
 
     /**
      * Displays a particular model.
      */
-    public function actionView() {
-      $model = $this->loadModel();
-      $this->render('view', array(
-        'model' => $model,
-      ));
+    public function actionView()
+    {
+        $model = $this->loadModel();
+        $this->render('view', array(
+            'model'=>$model,
+        ));
     }
 
     /**
      * Lists all models.
      */
-    public function actionIndex() {
-      $dataProvider = new CActiveDataProvider('User', array(
-        'criteria' => array(
-          'condition' => 'status>' . User::STATUS_BANNED,
-        ),
-        'pagination' => array(
-          'pageSize' => Yii::app()->controller->module->user_page_size,
-        ),
-      ));
+    public function actionIndex()
+    {
+        $dataProvider = new CActiveDataProvider('User', array(
+            'criteria'=>array(
+                'condition'=>'status>' . User::STATUS_BANNED,
+            ),
+            'pagination'=>array(
+                'pageSize'=>Yii::app()->controller->module->user_page_size,
+            ),
+        ));
 
-      $this->render('index', array(
-        'dataProvider' => $dataProvider,
-      ));
+        $this->render('index', array(
+            'dataProvider'=>$dataProvider,
+        ));
     }
 
     /**
      * Returns the data model based on the primary key given in the GET variable.
      * If the data model is not found, an HTTP exception will be raised.
      */
-    public function loadModel() {
-      if ($this->_model === null) {
-        if (isset($_GET['id']))
-          $this->_model = User::model()->findbyPk($_GET['id']);
-        if ($this->_model === null)
-          throw new CHttpException(404, 'The requested page does not exist.');
-      }
-      return $this->_model;
+    public function loadModel()
+    {
+        if ($this->_model === null) {
+            if (isset($_GET['id']))
+                $this->_model = User::model()->findbyPk($_GET['id']);
+            if ($this->_model === null)
+                throw new CHttpException(404, 'The requested page does not exist.');
+        }
+        return $this->_model;
     }
 
     /**
@@ -80,15 +86,16 @@
      * If the data model is not found, an HTTP exception will be raised.
      * @param integer the primary key value. Defaults to null, meaning using the 'id' GET variable
      */
-    public function loadUser($id = null) {
-      if ($this->_model === null) {
-        if ($id !== null || isset($_GET['id']))
-          $this->_model = User::model()->findbyPk($id !== null ? $id : $_GET['id']);
-        if ($this->_model === null)
-          throw new CHttpException(404, 'The requested page does not exist.');
-      }
-      return $this->_model;
+    public function loadUser($id = null)
+    {
+        if ($this->_model === null) {
+            if ($id !== null || isset($_GET['id']))
+                $this->_model = User::model()->findbyPk($id !== null ? $id : $_GET['id']);
+            if ($this->_model === null)
+                throw new CHttpException(404, 'The requested page does not exist.');
+        }
+        return $this->_model;
     }
 
-  }
+}
 
